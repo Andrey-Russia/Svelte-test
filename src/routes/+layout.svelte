cf1bfbd3a20b7a3861f8fcf8a77fed72d7d8a206
ndrey <kraevskoy04@gmail.com>
2023-06-01 12:06:45 +0300

    add pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<nav>
+    <div class="nav-container">
+        <!-- svelte-ignore missing-declaration -->
+        {#each nav as link}
+            <a href={link.href} class="link">{link.title}</a>
+        {/each}
+    </div>
+</nav>
+<div class="container">
+    <!-- Pages will be injected below -->
+    <slot />
+</div>
+
+<script>
+
+const nav = [
+{ title: 'Обо мне',
+ href: '/about' },
+{ title: 'Блог',
+ href: '/blog' },
+{ title: 'Портфолио',
+ href: '/project' },
+{ title: 'Контакты',
+ href: '/contact' },
+{ title: 'temp',
+ href: '/temp'}
+]
+</script>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
