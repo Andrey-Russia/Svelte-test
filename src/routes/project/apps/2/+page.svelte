6ac348cc77388e1029f1ab4840f1eb10b47a6e5c
ndrey <kraevskoy04@gmail.com>
2023-06-02 15:57:05 +0300

    routes

--- src/routes/project/apps/2/+page.svelte
+++ src/routes/project/apps/2/+page.svelte
@@ -4,7 +4,17 @@
 
 
 <main>
-     
+    <!-- svelte-ignore missing-declaration -->
+    <div class="table" items={items}>
+        <div class="title">
+            <div class="item"></div>
+            <div class="item"></div>
+        </div>
+        <div class="main" let:item={item}>
+            <div class="item date">{item.date}</div>
+            <div class="item name">{item.name}</div>
+        </div>
+    </div>
 </main>
 
 
