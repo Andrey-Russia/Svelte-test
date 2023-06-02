eeafadd40aac1e71dc0851a9c1b10b1ef1a588cf
ndrey <kraevskoy04@gmail.com>
2023-06-02 15:38:26 +0300

    2

---
+++ src/routes/project/apps/2/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementById('body').appendChild(table);
+</script>
+
+<main>
+
+</main>
+
+
+<style>
+
+
+</style>
